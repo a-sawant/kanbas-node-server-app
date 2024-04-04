5de79c1a5948ff1902aa69c9e2551800ea1ed2f9
Arnav Sawant <sawaarna9@gmail.com>
2024-04-03 21:54:40 -0400

    fixing route

--- Kanbas/courses/routes.js
+++ Kanbas/courses/routes.js
@@ -29,5 +29,17 @@ export default function CourseRoutes(app) {
     res.sendStatus(204);
   });
 
+  app.get("/api/courses/:id", (req, res) => {
+    const { id } = req.params;
+    const course = Database.courses
+      .find((c) => c._id === id);
+    if (!course) {
+      res.status(404).send("Course not found");
+      return;
+    }
+    res.send(course);
+  });
+
+
 
 }
